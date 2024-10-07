3d8f17b0bdf1f1873b32b82bcb6894c8ac4c15ec
Hikari06 <106168976+Hikari06@users.noreply.github.com>
2024-10-07 13:36:47 -0300

    final

---
+++ Assets/Scripts/ActivateCutScene.cs
@@ -0,0 +1,19 @@
+//Using para adicionar 
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class ActivateCutScene : MonoBehaviour
+{
+    [SerializeField] private PlayableDirector playableDirector;
+    // Start is called before the first frame update
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Cutscene Activated");
+            playableDirector.Play();
+            GetComponent<BoxCollider>().enabled = false;
+        }
+    }
+}
